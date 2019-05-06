126a497544687746335198481d3e0a678543f9b1
sinduja <sindhu.38@gmail.com>
2019-05-06 04:07:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,25 @@
 var chocolateBars=["snickers","hundred grand","kitkat","skittles"];
-fucntion addElementToBeginningOfArray(array,num)
+function addElementToBeginningOfArray(array,num)
 {
 
 newArr=  [1,...array];
 return newArr;
 }
-fucntion destructivelyAddElementToBeginningOfArray(array,num)
+function destructivelyAddElementToBeginningOfArray(array,num)
 {
   array.unshift(1);
+  return array;
+
+}
+function addElementToEndOfArray(array,num)
+{
+
+newArr=  [...array,1];
+return newArr;
+}
+function destructivelyAddElementToEndOfArray(array,num)
+{
+  array.push(1);
   return array;
 
 }
