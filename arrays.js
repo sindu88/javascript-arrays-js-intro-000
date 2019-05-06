e84742ca3c1371730e432e0aed1a72c5adbae043
sinduja <sindhu.38@gmail.com>
2019-05-06 04:17:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -37,7 +37,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array)
 }
 function removeElementFromBeginningOfArray(array)
 {
-  array.slice(1);
+array=  array.slice(1);
   return array;
 }
 
