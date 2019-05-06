d72d836ce4acfb2924fb279622fa41052cb7542b
sinduja <sindhu.38@gmail.com>
2019-05-06 04:04:50 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars=["snickers","hundred grand","kitkat","skittles"];
+fucntion addElementToBeginningOfArray(array,num)
+{
+
+newArr=  [1,...array];
+return newArr;
+}
+fucntion destructivelyAddElementToBeginningOfArray(array,num)
+{
+  array.unshift(1);
+  return array;
+  
+}
